1e85d5707aaa890cf74bd22b7b552eb74a425133
GodeGamleJacob <89967283+GodeGamleJacob@users.noreply.github.com>
2021-09-02 11:54:13 +0200

    Just work...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,15 @@
 package com.company;
 
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
         //Short message to screen
         System.out.println("Programming with Dat'erne is fun");
         System.out.println("Hello World");
+        System.out.println("Go see Marvels What If");
 
     }
 }
