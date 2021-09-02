e888098cf9a3fbb17d9e178b9d64c755b1d06afc
GodeGamleJacob <jaco236u@edu.easj.dk>
2021-09-02 11:27:20 +0200

    Just work...

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Short message to screen
+        System.out.println("Programming with Dat'erne is fun");
+        System.out.println("Hello World");
+
+    }
+}
